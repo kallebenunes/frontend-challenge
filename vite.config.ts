e51c14c99b1ee8970c7d118d187897701b6fc225
Kallebe Nunes <kallebe.nunes@trinto.com.br>
2024-12-05 15:51:03 -0300

    chore: configure alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Alias for 'src' directory
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/_common.scss" as *; @use "@/styles/_variables.scss" as *;`,
+      },
+    },
+  },
 });
